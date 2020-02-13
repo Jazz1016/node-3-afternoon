78ee013f4c69ada4c2e9fd50cb75875a12462741
Jazz1016 <james.q.lea@gmail.com>
2020-02-12 17:29:35 -0700

    project done

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE product(
+-- product_id SERIAL NOT NULL,
+-- name VARCHAR(40) NOT NULL,
+-- desription VARCHAR(80),
+-- price INTEGER,
+-- image_url TEXT
+-- )
+
+-- INSERT INTO product (name, desription, price, image_url) values ('mouse', 'used for moving cursor on computer', 4, 'https://target.scene7.com/is/image/Target/GUEST_62a4c76c-20de-4f4b-a346-9d161208fbdf?wid=488&hei=488&fmt=pjpeg')
